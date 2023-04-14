34b66cfb16b081edf5d9abb7474566834253e2bd
chenyaofo <chenyaofo@gmail.com>
2023-04-14 12:04:59 +0800

    add logging.

---
+++ torch4x/logging.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import typing
+import logging
+import glob
+import zipfile
+
+from .distributed import is_rank_0
+
+_logger = logging.getLogger(__name__)
+
+
+class LogExceptionHook(object):
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
+
+    def __call__(self, exc_type, exc_value, traceback):
+        self.logger.exception("Uncaught exception", exc_info=(
+            exc_type, exc_value, traceback))
+
+
+FORMAT = "[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)d:%(funcName)s] %(message)s"
+
+
+def init_logger(
+    rank: int,
+    filenmae: str = None
+) -> None:
+    if rank != 0:
+        logging.basicConfig(level=logging.CRITICAL)
+    else:
+        _logger = logging.getLogger(__name__)
+        for handler in _logger.handlers[:]:  # make a copy of the list
+            _logger.removeHandler(handler)
+
+        formatter = logging.Formatter(FORMAT)
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        handlers = [console_handler]
+        if filenmae is not None:
+            file_handler = logging.FileHandler(filenmae)
+            file_handler.setFormatter(formatter)
+            handlers.append(file_handler)
+
+        level = getattr(logging, os.environ.get("LOG_LEVEL", "INFO"))
+
+        logging.basicConfig(
+            level=level,
+            handlers=handlers
+        )
+
+        sys.excepthook = LogExceptionHook(_logger)
+
+
+def create_code_snapshot(
+    name: str,
+    include_suffix: typing.List[str],
+    source_directory: str,
+    store_directory: str
+) -> None:
+    if is_rank_0():
+        if store_directory is None:
+            return
+        with zipfile.ZipFile(os.path.join(store_directory, "{}.zip".format(name)), "w") as f:
+            for suffix in include_suffix:
+                for file in glob.glob(os.path.join(source_directory, "**", "*{}".format(suffix)), recursive=True):
+                    f.write(file, os.path.join(name, file))
