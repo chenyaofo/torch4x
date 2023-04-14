4d9a660a5f2204c084669183c64ee41291858350
chenyaofo <chenyaofo@gmail.com>
2023-04-14 17:17:10 +0800

    add common, metrics.

--- torch4x/__init__.py
+++ torch4x/__init__.py
@@ -1,2 +1,48 @@
 
+from .distributed import (
+    is_dist_avail_and_init,
+    local_rank,
+    rank,
+    world_size,
+    is_rank_0,
+    torchsave
+)
+
+from .logging import init_logger, create_code_snapshot
+
+from .typed_args import TypedArgs
+
+from .metrics import (
+    AccuracyMetric,
+    AverageMetric,
+    EstimatedTimeArrival
+)
+
+from .common import (
+    generate_random_seed,
+    set_reproducible,
+    set_cudnn_auto_tune,
+    disable_debug_api,
+    compute_nparam,
+    compute_flops,
+    get_last_commit_id,
+    get_branch_name,
+    get_all_gpus_memory_info_from_nvidiasmi,
+    get_free_port,
+    GradientAccumulator,
+    only_rank_0,
+    ModelEma,
+    unwarp_module,
+    patch_download_in_cn,
+    MetricsStore,
+    StateCheckPoint,
+    ThroughputTester,
+    time_enumerate,
+    set_active_device,
+    get_active_device,
+    save_uuid,
+    apply_modifications,
+    load_modules
+)
+    
 __version__ = "0.0.1"
